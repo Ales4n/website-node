03f30c194e3f6cf7902616de3b95e0e80d506451
Alessandro Sansossio <san07ar@gmail.com>
2020-09-03 17:53:59 +0200

    files moved from folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { get } = require('http');
 
 //settings
 app.set('port', 3000)
-app.set('views', path.join(__dirname, 'public', 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs')
 
@@ -13,6 +13,9 @@ app.set('view engine', 'ejs')
 
 //routes
 app.use(require('./routes/index'))
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Listenign the server
 app.listen(app.get('port'), ()  => {
